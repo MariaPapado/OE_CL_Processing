e6d671384b128031e7ba94502f04f7d3c9239d60
MariaPapado <maria.papadomanolaki@orbitaleye.nl>
2025-06-02 14:45:46 +0000

    split

--- img_split.py
+++ img_split.py
@@ -1,6 +1,8 @@
 import cv2
 from pathlib import Path
 from tqdm import tqdm
+import os
+import shutil
 
 def parse_yolo_label(label_line):
     """Parse a YOLO format label line"""
@@ -27,6 +29,7 @@ def absolute_to_yolo(x1, y1, x2, y2, img_width, img_height):
 
 def crop_and_adjust_labels(image_path, label_path, crop_size, padding, output_dir):
     """Crop image into squares and adjust labels accordingly"""
+
     # Read image
     image = cv2.imread(str(image_path))
     if image is None:
@@ -93,8 +96,8 @@ def crop_and_adjust_labels(image_path, label_path, crop_size, padding, output_di
             # Save crop and labels
             if crop_labels or True:  # Save even if no labels
                 stem = image_path.stem
-                crop_image_path = output_dir / f"{stem}_crop_{crop_count}.png"
-                crop_label_path = output_dir / f"{stem}_crop_{crop_count}.txt"
+                crop_image_path = output_dir / 'images/' / f"{stem}_crop_{crop_count}.png"
+                crop_label_path = output_dir / 'labels/' f"{stem}_crop_{crop_count}.txt"
                 
                 cv2.imwrite(str(crop_image_path), crop)
                 
@@ -109,10 +112,21 @@ def process_dataset(image_dir, label_dir, output_dir, crop_size, padding):
     image_dir = Path(image_dir)
     label_dir = Path(label_dir)
     output_dir = Path(output_dir)
+
+    output_dir_images = output_dir / 'images/'
+    if os.path.exists(output_dir_images):
+        shutil.rmtree(output_dir_images)
+    os.makedirs(output_dir_images)
+
+    output_dir_labels = output_dir / 'labels/'
+    if os.path.exists(output_dir_labels):
+        shutil.rmtree(output_dir_labels)
+    os.makedirs(output_dir_labels)
+
     output_dir.mkdir(parents=True, exist_ok=True)
     
     # Find all image files
-    image_extensions = {'.png', '.jpg', '.jpeg', '.bmp', '.tiff'}
+    image_extensions = {'.png', '.jpg', '.jpeg', '.bmp', '.tiff', '.tif'}
     image_files = [f for f in image_dir.iterdir() if f.suffix.lower() in image_extensions]
 
     for image_path in tqdm(image_files, desc="Processing images"):
@@ -125,9 +139,9 @@ def process_dataset(image_dir, label_dir, output_dir, crop_size, padding):
 def main():
     
     CONFIG = {
-        "image_dir": "/cephfs/work/rithvik/datasets/datasets/NatCombined/images/train/",
-        "label_dir": "/cephfs/work/rithvik/datasets/datasets/NatCombined/labels/train/",
-        "output_dir": "/cephfs/work/rithvik/datasets/datasets/NatCombined/TEST_cropped/",
+        "image_dir": "./bhe_data/train/images/",
+        "label_dir": "./bhe_data/train/labelTxt/",
+        "output_dir": "./bhe_data/cropped/",
         "crop_size": 1024,
         "padding": 50
     }
