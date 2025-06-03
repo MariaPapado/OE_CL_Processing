f6ca3ea4f301ee23f5b03eb7bb03a7f3caa3242e
MariaPapado <maria.papadomanolaki@orbitaleye.nl>
2025-06-03 09:12:12 +0000

    commit

--- myautosplit.py
+++ myautosplit.py
@@ -2,5 +2,5 @@ from ultralytics.data.split import autosplit
 from pathlib import Path
 
 # assuming a path to images
-images = Path('./bhe_data/whole_data')  ##dir with /images /labels
+images = Path('./bhe_data/cropped/')  ##dir with /images /labels
 autosplit(images, weights=(0.7,0.15,0.15)) # train, validation, test splits
